8570a780b315d21a2cc296eae50610ba81e99896
Cécilia Bossard <cecilia.bossard@gmail.com>
2016-09-12 16:22:23 +0200

    Ajout map

--- app/pages/map/map.ts
+++ app/pages/map/map.ts
@@ -1,43 +1,14 @@
 import { Component } from '@angular/core';
 
-import { ConferenceData } from '../../providers/conference-data';
+
+import { MapService } from '../../providers/map-service/map-service';
 
 
 @Component({
-  templateUrl: 'build/pages/map/map.html'
+  templateUrl: 'build/pages/map/map.html',
+  providers: [MapService]
 })
 export class MapPage {
-  constructor(public confData: ConferenceData) {}
+  constructor(private mapService: MapService) {}
 
-/*  ionViewLoaded() {
-    this.confData.getMap().then(mapData => {
-      let mapEle = document.getElementById('map');
-
-      let map = new google.maps.Map(mapEle, {
-        center: mapData.find(d => d.center),
-        zoom: 16
-      });
-
-      mapData.forEach(markerData => {
-        let infoWindow = new google.maps.InfoWindow({
-          content: `<h5>${markerData.name}</h5>`
-        });
-
-        let marker = new google.maps.Marker({
-          position: markerData,
-          map: map,
-          title: markerData.name
-        });
-
-        marker.addListener('click', () => {
-          infoWindow.open(map, marker);
-        });
-      });
-
-      google.maps.event.addListenerOnce(map, 'idle', () => {
-        mapEle.classList.add('show-map');
-      });
-
-    });
-  }*/
 }
